9bb714849dd9d890a0a7ac216b2591f294baed4c
blarsonmoore <blarsonmoore@gmail.com>
2018-07-31 23:20:36 -0600

    first commit

---
+++ db/seeds.sql
@@ -0,0 +1,14 @@
+
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ('Bacon');
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ('Vegie');
+INSERT INTO burgers
+  (burger_name, devoured)
+VALUES
+  ('Extra Cheese');
+
